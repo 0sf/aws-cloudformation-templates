cd4f66c718d0f7ef2ae43eedc7de48ed8f049c5b
0sf <nsankalpa7@gmail.com>
2020-04-25 23:04:39 +0530

    added-lambda-edge-s3-code-bucket

---
+++ aws-sample-website-template/phase3/files/lambda/index.js
@@ -0,0 +1,47 @@
+"use strict";
+exports.handler = (event, context, callback) => {
+  var request = event.Records[0].cf.request;
+  const headers = request.headers;
+
+  const reactVersion = "version=react";
+  const angularVersion = "version=angular";
+
+  const reactVersionDomain = "vesiononebucket3101.s3-website-us-east-1.amazonaws.com";
+  const angularVersionDomain = "angularVersionbucket3101.s3-website-us-east-1.amazonaws.com";
+
+  let domain = "";
+
+  if (headers.cookie) {
+    for (let i = 0; i < headers.cookie.length; i++) {
+      if (headers.cookie[i].value.indexOf(reactVersion) >= 0) {
+        domain = reactVersionDomain;
+        break;
+      } else if (headers.cookie[i].value.indexOf(angularVersion) >= 0) {
+        domain = angularVersionDomain;
+        break;
+      }
+    }
+
+    request.origin = {
+      custom: {
+        domainName: domain,
+        port: 80,
+        protocol: "http",
+        path: "",
+        sslProtocols: ["TLSv1", "TLSv1.1"],
+        readTimeout: 5,
+        keepaliveTimeout: 5,
+        customHeaders: {},
+      }
+    };
+
+    request.headers["host"] = [{ key: "host", value: domain }];
+    callback(null, request);
+  } else {
+    callback(null, request);
+    return;
+  }
+};
+
+
+// 	d3bck4t5p2hriy.cloudfront.net
